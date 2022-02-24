e179b50619246a7250dfb8f8c90dc257f1640499
Brian Cardarella <bcardarella@gmail.com>
2022-02-24 10:28:55 -0500

    Support reverse animation range
    * fix bug where impossible ranges should return empty list

--- test/easing/animation_range_test.exs
+++ test/easing/animation_range_test.exs
@@ -12,4 +12,26 @@ defmodule Easing.AnimationRangeTest do
     assert Enum.at(list, 0) == 0.0
     assert Enum.at(list, length(list) - 1) == 1.0
   end
+
+  test "reverse ranges work with negative steps" do
+    range = %AnimationRange{first: 10, last: 0, step: -1}
+
+    list = Enum.to_list(range)
+
+    assert list == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+  end
+
+  test "impossible ranges result in empty lists" do
+    range = %AnimationRange{first: 1, last: 0, step: 1}
+
+    list = Enum.to_list(range)
+
+    assert list == []
+
+    range = %AnimationRange{first: 0, last: 1, step: -1}
+
+    list = Enum.to_list(range)
+
+    assert list == []
+  end
 end
